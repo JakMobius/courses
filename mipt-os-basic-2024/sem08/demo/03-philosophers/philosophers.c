a400717fac975690f63ce73d3b1c3380594038bf
Artem Klimov <jakmobius@gmail.com>
2024-11-07 13:01:37 +0300

    Revert philosophers.c change

--- mipt-os-basic-2024/sem08/demo/03-philosophers/philosophers.c
+++ mipt-os-basic-2024/sem08/demo/03-philosophers/philosophers.c
@@ -2,23 +2,13 @@
 
 void start_eating(philosopher_t *philosopher)
 {
-    if (philosopher->id % 2 == 0) {
-        capture_right_fork(philosopher);
-        capture_left_fork(philosopher);
-    } else {
-        // TODO: fix the deadlock
-        capture_left_fork(philosopher);
-        capture_right_fork(philosopher);
-    }
+    // TODO: fix the deadlock
+    capture_left_fork(philosopher);
+    capture_right_fork(philosopher);
 }
 
 void stop_eating(philosopher_t *philosopher)
 {
-    if (philosopher->id % 2 != 0) {
-        release_left_fork(philosopher);
-        release_right_fork(philosopher);
-    } else {
-        release_right_fork(philosopher);
-        release_left_fork(philosopher);
-    }
+    release_left_fork(philosopher);
+    release_right_fork(philosopher);
 }
